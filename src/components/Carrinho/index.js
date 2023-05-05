eb39dde0f52f698e10a252129ced706906bf2694
Isabellaizira <izidoroisabella@gmail.com>
2023-05-05 13:45:40 -0300

    att

--- src/components/Carrinho/index.js
+++ src/components/Carrinho/index.js
@@ -1,11 +1,12 @@
-
+import './carrinho.css'
 import '../Header/header.css'
-
+// import manutenção from '../Carrinho/imagens/manutenção'
 
 export default function Header() {
     return (
         <div>
-          <h1>Em Manutenção</h1>
+          <h2>Em Manutenção</h2>
+          {/* <img src={manutenção} alt="Manutenção"  ></img>         */}
             </div>
           )
 }
