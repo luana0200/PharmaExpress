cdf91e90a3bc340844881b3bc1a9195b33ed1afb
Isabellaizira <izidoroisabella@gmail.com>
2023-05-05 14:52:46 -0300

    att

--- src/components/Carrinho/index.js
+++ src/components/Carrinho/index.js
@@ -1,12 +1,18 @@
 import './carrinho.css'
 import '../Header/header.css'
 import manutencao from "./imagens/manutencao.png"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Header() {
     return (
-        <div>
+        <Container>
+          <Row md= {4}>
+           
           <h2>Carrinho</h2>
-          <img src={manutencao} alt="Manutencao" />
-            </div>
+          <Col><img src={manutencao} alt="Manutencao" /></Col>
+          </Row>
+            </Container>
           )
 }
