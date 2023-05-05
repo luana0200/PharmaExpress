747d49d1bcde500c5a1811ddfdcf49128cf19d34
luana0200 <luana7forte@gmail.com>
2023-05-05 14:21:45 -0300

    att

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,25 +6,42 @@ export default function Header() {
         <div className='container-fluid'>
             <div className='row'>
 
-                <div className='col-5'>
-                    <div className='botao2'>
-                        <Link to='/'><button></button></Link>
-                    </div>
-                </div>
+                <div class="container-fluid">
+                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                        <a class="navbar-brand"> <Link to='/'><button></button></Link></a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup">
+                            <span class="navbar-toggler-icon">
+                                <div class="navbar-nav" data-bs-toggle="collapse" aria-controls="navbarNavAltMarkup">
+                                    <a  ><button><Link to='/'>🏠</Link><br /> </button></a>
+                                    <a ><button ><Link to='/Baby'>Baby</Link><br /></button></a>
+                                    <a ><button ><Link to='/HPessoal'>Higiene Pessoal</Link><br /></button></a>
+                                    <a  ><button > <Link to='/Medicamentos'>Medicamentos</Link><br /></button></a>
+                                    <a > <button > <Link to='/Beleza'>Beleza</Link><br /></button></a>
+                                    <a  ><button ><Link to='Cadastro'>Cadastro</Link><br /></button></a>
+                                    <a >  <button ><Link to='Carrinho'> 🛒 </Link><br /></button></a>
 
-                <div className='col-sm'>
-                    <button><Link to='/'>🏠</Link><br /> </button>
-                    <button ><Link to='/Baby'>Baby</Link><br /></button>
-                    <button ><Link to='/HPessoal'>Higiene Pessoal</Link><br /></button>
-                    <button><Link to='/Medicamentos'>Medicamentos</Link><br /></button>
-                    <button ><Link to='/Beleza'>Beleza</Link><br /></button>
-                    <button ><Link to='Cadastro'>Cadastro</Link><br /></button>
-                    <button ><Link to='Carrinho'> 🛒 </Link><br /></button>
-                </div>
+                                </div>
+                            </span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <a class="nav-link" ><button><Link to='/'>🏠</Link><br /> </button></a>
+                                <a class="nav-link"><button ><Link to='/Baby'>Baby</Link><br /></button></a>
+                                <a class="nav-link" ><button ><Link to='/HPessoal'>Higiene Pessoal</Link><br /></button></a>
+                                <a class="nav-link" ><button > <Link to='/Medicamentos'>Medicamentos</Link><br /></button></a>
+                                <a class="nav-link"> <button > <Link to='/Beleza'>Beleza</Link><br /></button></a>
+                                <a class="nav-link" ><button ><Link to='Cadastro'>Cadastro</Link><br /></button></a>
+                                <a class="nav-link">  <button ><Link to='Carrinho'> 🛒 </Link><br /></button></a>
 
+                            </div>
+                        </div>
+                    </nav>
+                </div>
             </div>
+        </div >
 
-        </div>
+
+
     )
 
 }
