ed4c79f48c277b799023d14bf246ed72630f1988
luana0200 <luana7forte@gmail.com>
2023-05-08 16:40:45 -0300

    aa

--- src/pages/baby/index.js
+++ src/pages/baby/index.js
@@ -32,7 +32,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px" src={ShampooJonhson} alt="Shampoo" />
                     <Card.Body>
                         <Card.Title><h2>Shampoo Jonhson baby 200 ml</h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/ShampooJonhson'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -42,7 +42,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px" src={Mamadeira} alt="Mamadeira" />
                     <Card.Body>
                         <Card.Title><h2> Mamadeira Rosa 250 ml</h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/MamaRosa'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -53,7 +53,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px" src={Lenco} alt="Lenco" />
                     <Card.Body>
                         <Card.Title><h2>Lenço Umidecido Jonhson Baby </h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/lenco'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -63,7 +63,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px" src={lenco} alt="Locao" />
                     <Card.Body>
                         <Card.Title><h2>Lenço Umidecido  Pampers 48 unidade</h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/Lenco2' ><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -73,7 +73,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px" src={Locao} alt="Locao" />
                     <Card.Body>
                         <Card.Title><h2> Loção hidratante Jonhson baby </h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/Locao' ><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -84,7 +84,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px" src={Fralda} alt="Fralda" />
                     <Card.Body>
                         <Card.Title><h2>Fralda Pampers </h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/Fralda' ><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -94,7 +94,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px" src={Sabonet} alt="Sabonet" />
                     <Card.Body>
                         <Card.Title><h2> Sabonete Liquido Jonhson baby  </h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/Sabao'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -104,7 +104,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px"src={Pomada} alt="Pomada" />
                     <Card.Body>
                         <Card.Title><h2>  Pomada Hipogloss </h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/Sabao'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -114,7 +114,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px"src={Chupetas} alt="Chupetas" />
                     <Card.Body>
                         <Card.Title><h2> Chupetas</h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/Chupetas'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -124,7 +124,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px"src={Formula} alt="Formula" />
                     <Card.Body>
                         <Card.Title><h2>  Formula Aptamil s</h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/Aptamil'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -134,7 +134,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px"src={Rosa} alt="Rosa" />
                     <Card.Body>
                         <Card.Title><h2>   Kit Escova Rosa</h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/rosa'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -144,7 +144,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px"src={Kit} alt="Kit Azul" />
                     <Card.Body>
                         <Card.Title><h2>   Kit Escova Azul</h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/kit'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -153,7 +153,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px"src={mama} alt="mama" />
                     <Card.Body>
                         <Card.Title><h2>   Mamadeira Azul 250 ml</h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/Mamadeira'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -163,7 +163,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px" src={sbonete} alt="sabonete" />
                     <Card.Body>
                         <Card.Title><h2>    Sabonete em Barra Jonhson Baby</h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/Sabonete'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
@@ -173,7 +173,7 @@ export default function Baby() {
                     <Card.Img variant="top" width="220px" src={talco} alt='talco' />
                     <Card.Body>
                         <Card.Title><h2>   Talco Johnson</h2></Card.Title>
-                        <div className='Container details'>
+                        <div className='details'>
                             <Link to='/talco'><Button variant="secondary">Detalhes</Button></Link>
                         </div>
                     </Card.Body>
