0fd9bfcec87bb7b0695f9ff50e8dd7fa55d6c0f3
lorraynecheerry <lorraynesabino554@gmail.com>
2023-05-09 17:20:21 -0300

    aa

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,29 +8,29 @@ import Navbar from 'react-bootstrap/Navbar';
 
 export default function Header() {
     return (
-      
-            <Container fluid>
-                    <Navbar expand="xxl" className='nave'>
-                        <Container fluid className='dark'>
-                            <img src={logo} alt='logo' />
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" id='Row' />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="Container button">
-                                    <Nav.Link href="/"><Button variant="secondary">🏠 </Button></Nav.Link>
-                                    <Nav.Link href="/Baby"><Button variant="secondary" >Baby</Button ></Nav.Link>
-                                    <Nav.Link href="/HPessoal"><Button variant="secondary" >Higiene Pessoal</Button ></Nav.Link>
-                                    <Nav.Link href="/Medicamentos"><Button variant="secondary"> Medicamentos</Button ></Nav.Link>
-                                    <Nav.Link href="/Beleza"><Button variant="secondary" >Beleza</Button ></Nav.Link>
-                                    <Nav.Link href="/Cadastro"><Button variant="secondary" >Cadastro</Button ></Nav.Link>
-                                    <Nav.Link href="/Carrinho"><Button variant="secondary">🛒 </Button></Nav.Link>
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
-            </Container>
+
+        <Container fluid>
+            <Navbar expand="xxl" className='nave'>
+                <Container fluid className='dark'>
+                    <img src={logo} alt='logo' />
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" id='Row' />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="Container button">
+                            <Nav.Link href="/"><Button variant="secondary">🏠 </Button></Nav.Link>
+                            <Nav.Link href="/Baby"><Button variant="secondary" >Baby</Button ></Nav.Link>
+                            <Nav.Link href="/HPessoal"><Button variant="secondary" >Higiene Pessoal</Button ></Nav.Link>
+                            <Nav.Link href="/Medicamentos"><Button variant="secondary"> Medicamentos</Button ></Nav.Link>
+                            <Nav.Link href="/Beleza"><Button variant="secondary" >Beleza</Button ></Nav.Link>
+                            <Nav.Link href="/Cadastro"><Button variant="secondary" >Cadastro</Button ></Nav.Link>
+                            <Nav.Link href="/Carrinho"><Button variant="secondary">🛒 </Button></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
 
 
-            )
+    )
 }
 
 
