ac7569bc0fa4b403dc76e127df416fa346f6a18a
lorraynecheerry <lorraynesabino554@gmail.com>
2023-05-10 14:36:33 -0300

    att

--- src/pages/HPessoal/Detalhes/DesoF.js
+++ src/pages/HPessoal/Detalhes/DesoF.js
@@ -5,15 +5,19 @@ import Container from 'react-bootstrap/esm/Container'
 import Button from 'react-bootstrap/esm/Button'
 import Card from 'react-bootstrap/Card'
 
-export default function DesoF(){
-    return(
+export default function DesoF() {
+    return (
         <Container >
             <Card className='m-2, p-2'>
                 <Card.Img className='detail' variant="top" src={desoF} alt="desodorante" />
-            <h4>Desodorante Rexona Fem 200 ml</h4><br />
+                <h4>Desodorante Rexona Fem 200 ml</h4><br />
+                Indicado para todos os tipos de pele.
+                Protege 3 vezes mais do que os antitranspirantes comuns.
+                Controla o suor excessivo e previne o mau odor por 96 horas.
+                Proporciona frescor, conforto e segurança.
 
-            <h1>R$ 16,99</h1>
-            <div className='Container llog'>
+                <h1>R$ 16,99</h1>
+                <div className='Container llog'>
                     <Link to='/Compra'> <Button variant="secondary">COMPRAR</Button></Link>
                 </div>
             </Card>
--- src/pages/HPessoal/Detalhes/EscovaColgate.js
+++ src/pages/HPessoal/Detalhes/EscovaColgate.js
@@ -11,6 +11,9 @@ export default function EscovaColgate(){
             <Card className='m-2, p-2'>
                 <Card.Img className='detail' variant="top" src={escovacarvao} alt="escova de dente" />
                 <h4> Escova Dental Colgate 4 unidade  </h4>
+                A Escova Dental Colgate Preta é indicada para o cuidado e proteção da saúde bucal. 
+                O produto possui as cerdas ultramacias e infundidas no carvão, que limpam os dentes profundamente, 
+                sem causar desconfortos. Uso diário. A embalagem contém 4 unidades.
                
                 <h1> R$ 20,90</h1>
                 <div className='Container llog'>
