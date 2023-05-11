6053b8e137544df81988f7564d70424ff3396db1
lorraynecheerry <lorraynesabino554@gmail.com>
2023-05-11 13:41:07 -0300

    aaa

--- src/components/Compra/Compra.js
+++ src/components/Compra/Compra.js
@@ -2,50 +2,38 @@ import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 // import '../Header/header.css'
 import './compra.css'
-import { IMaskInput } from 'react-imask';
-import Form from 'react-bootstrap/Form';
-import Container from "react-bootstrap/esm/Container";
+import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
+
 
 export default function Compra() {
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
 
-
     return (
         <Container fluid>
-            <div className='comprando'>
+            <div className='compra'>
+                <div>
+                    <b>Já sou cliente</b>
+                    <p>Faça o login para acelerar a compra:</p>
+                    <label>E-mail</label>
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} /><br /><p />
 
-                <b>Já sou cliente</b>
-                <p>Faça o login para acelerar a compra:</p>
+                    <label>Senha</label>
+                    <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} /> <br />
+                    <br /><Link to='/Carrinho'><Button variant="secondary">Login</Button> </Link>
+                </div>
 
-                <Form.Group className="comprinha">
-                    <Form.Label>E-mail</Form.Label>
-                    <Form.Control
-                        as={IMaskInput}
-
-                        placeholder="Digite seu E-mail"
-                    />
-                </Form.Group>
-
-                <Form.Group className="comprinha">
-                    <Form.Label>Senha</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Digite sua Senha"
-                    />
-                </Form.Group><br />
-
-                <Button variant='secondary' type="submit">Enviar</Button>
-
-                <Form.Group className='cadastrando'>
+                <div className='comprar'>
                     <b>Ainda não sou cliente</b>
                     <p>Cadastre-se para finalizar sua compra.</p>
-                </Form.Group>
-
-                <Link to='/Cadastro'><Button variant='secondary' type="submit">Cadastre-se</Button></Link>
+                    <Link to='/Cadastro'><Button variant="secondary">Cadastre-se</Button></Link>
+                </div>
 
             </div>
+
         </Container>
+
     )
+
 }
