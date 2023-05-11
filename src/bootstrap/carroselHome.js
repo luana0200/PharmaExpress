d845fb3f0fe4840d6afb8da9d6f53d6639dfe185
luana0200 <luana7forte@gmail.com>
2023-05-11 15:08:49 -0300

    aa

--- src/bootstrap/carroselHome.js
+++ src/bootstrap/carroselHome.js
@@ -1,30 +1,39 @@
 import Carousel from 'react-bootstrap/Carousel';
+import React, { useState } from 'react';
 
 import './style.css'
 import escovadental from '../bootstrap/imagens/escovadente.webp'
 import Pampers from '../bootstrap/imagens/Pampers.jpg'
 import Eudora from '../bootstrap/imagens/mae.jpg'
 
+
 export default function CarroselHome() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
     return (
-        <Carousel>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item>
-            <div class="carroselhome">
-                        <img className="d-block w-100" src={escovadental} />
-                    </div>
+                <div class="carroselhome">
+                    <img className="d-block w-100" src={escovadental} />
+                </div>
             </Carousel.Item>
 
             <Carousel.Item>
-                    <div class="carroselhome">
-                        <img className="d-block w-100" src={Pampers} />
-                    </div>
+                <div class="carroselhome">
+                    <img className="d-block w-100" src={Pampers} />
+                </div>
 
             </Carousel.Item>
             <Carousel.Item>
-                    <div class="carroselhome">
-                        <img className="d-block w-100" src={Eudora} />
-                    </div>
+                <div class="carroselhome">
+                    <img className="d-block w-100" src={Eudora} />
+                </div>
             </Carousel.Item>
         </Carousel>
     );
 }
+
