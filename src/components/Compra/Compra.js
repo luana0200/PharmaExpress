702c745308eed546db99cb3a5943d84b700bd7e3
lorraynecheerry <lorraynesabino554@gmail.com>
2023-05-11 11:40:41 -0300

    aaa

--- src/components/Compra/Compra.js
+++ src/components/Compra/Compra.js
@@ -15,35 +15,36 @@ export default function Compra() {
     return (
         <Container fluid>
             <div className='comprando'>
-                
-                    <b>Já sou cliente</b>
-                    <p>Faça o login para acelerar a compra:</p>
-                    
-                    <Form.Group className="comprinha">
-                        <Form.Label>E-mail</Form.Label>
-                        <Form.Control
-                            as={IMaskInput}
-                           
-                            placeholder="Digite seu E-mail"
-                        />
-                    </Form.Group>
 
-               <Form.Group className="comprinha">
-                        <Form.Label>Senha</Form.Label>
-                        <Form.Control
-                            type="password"
-                            placeholder="Digite sua Senha"
-                        />
-                    </Form.Group><br/>
+                <b>Já sou cliente</b>
+                <p>Faça o login para acelerar a compra:</p>
 
-                    <p>   <Button variant='secondary' type="submit">Enviar</Button></p> 
+                <Form.Group className="comprinha">
+                    <Form.Label>E-mail</Form.Label>
+                    <Form.Control
+                        as={IMaskInput}
 
-                     <Form.Group className='cadastrando'>
+                        placeholder="Digite seu E-mail"
+                    />
+                </Form.Group>
+
+                <Form.Group className="comprinha">
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Digite sua Senha"
+                    />
+                </Form.Group><br />
+
+                <Button variant='secondary' type="submit">Enviar</Button>
+
+                <Form.Group className='cadastrando'>
                     <b>Ainda não sou cliente</b>
                     <p>Cadastre-se para finalizar sua compra.</p>
-                    </Form.Group>
-                    <Link to='/Cadastro'><Button variant="secondary">Cadastre-se</Button></Link>
-                
+                </Form.Group>
+
+                <Link to='/Cadastro'><Button variant='secondary' type="submit">Cadastre-se</Button></Link>
+
             </div>
         </Container>
     )
