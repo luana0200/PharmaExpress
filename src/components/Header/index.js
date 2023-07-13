d39f4c8bb4c6225497fe771531b228984de8c9cd
lorraynecheerry <lorraynesabino554@gmail.com>
2023-07-13 14:29:01 -0300

    aaa

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import './header.css'
 import { Link } from 'react-router-dom'
-
+import { BsCart2,BsFillHouseFill } from "react-icons/bs";
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button';
 import logo from './imagens/logo1.png'
@@ -18,13 +18,17 @@ export default function Header() {
                     <Navbar.Collapse id="basic-navbar-nav">
                         {/* <div className='center'> */}
                         <Nav className="Container button" >
-                            <Nav.Link href="/"><Button variant="secondary">🏠 </Button></Nav.Link>
+                            <Nav>
+                            <Nav.Link href="/"><Button variant="secondary"><BsFillHouseFill size='1.5rem' color='black'
+                            /></Button></Nav.Link>
+                            </Nav>
+
                             <Nav.Link href="/Baby"><Button variant="secondary" >Baby</Button ></Nav.Link>
                             <Nav.Link href="/HPessoal"><Button variant="secondary" >Higiene Pessoal</Button ></Nav.Link>
                             <Nav.Link href="/Medicamentos"><Button variant="secondary"> Medicamentos</Button ></Nav.Link>
                             <Nav.Link href="/Beleza"><Button variant="secondary" >Beleza</Button ></Nav.Link>
                             <Nav.Link href="/Cadastro"><Button variant="secondary" >Cadastro</Button ></Nav.Link>
-                            <Nav.Link href="/Carrinho"><Button variant="secondary">🛒 </Button></Nav.Link>
+                            <Nav.Link href="/Carrinho"><Button variant="secondary">< BsCart2 size='1.5rem' color='white'/> </Button></Nav.Link>
                         </Nav>
                         {/* </div> */}
                     </Navbar.Collapse>
