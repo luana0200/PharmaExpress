e8d271ca67a7e8732cdf52e01cd18d6994bde01b
luana0200 <luana7forte@gmail.com>
2023-11-06 12:28:20 -0300

    att

--- frontend/src/components/Carrinho/index.js
+++ frontend/src/components/Carrinho/index.js
@@ -1,11 +1,7 @@
 import './carrinho.css'
 import '../Header/header.css'
-import { useState, useEffect } from 'react'
-import apiBack from '../../services/apiBack'
 
-import manutencao from "./imagens/manutencao.png"
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 
 export default function Carrinho() {
 
