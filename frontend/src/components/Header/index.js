a6f514d0cb648c8db4b34c97129e11523a01e12b
luana0200 <luana7forte@gmail.com>
2023-11-09 15:08:41 -0300

    att

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,32 +1,36 @@
 import './header.css'
 import { Link } from 'react-router-dom'
-import { BsCart2, BsFillHouseFill, BsPersonFill } from "react-icons/bs";
+import { BsCart2, BsFillHouseFill, BsPersonFill } from 'react-icons/bs'
+import { LiaAndroid } from 'react-icons/lia'
 import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button'
 import logo from './imagens/logo1.png'
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
 
 export default function Header() {
     return (
 
         <Container fluid>
-            <Navbar expand="xxl" className='nave'>
+            <Navbar expand='xxl' className='nave'>
                 <Container fluid className='dark'>
                     <Link to='/'><img src={logo} alt='logo' /></Link>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" id='Row' />
-                    <Nav className="Container Menu" >
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="Container button" >
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' id='Row' />
+                    <Nav className='Container Menu' >
+                        <Navbar.Collapse id='basic-navbar-nav'>
+                            <Nav className='Container button' >
 
-                                <Nav.Link href="/Baby"><Button variant="secondary" >Baby</Button ></Nav.Link>
-                                <Nav.Link href="/HPessoal"><Button variant="secondary" >Higiene Pessoal</Button ></Nav.Link>
-                                <Nav.Link href="/Medicamentos"><Button variant="secondary"> Medicamentos</Button ></Nav.Link>
-                                <Nav.Link href="/Beleza"><Button variant="secondary" >Beleza</Button ></Nav.Link>
+                                <Nav.Link href='/Baby'><Button variant='secondary' >Baby</Button ></Nav.Link>
+                                <Nav.Link href='/HPessoal'><Button variant='secondary' >Higiene Pessoal</Button ></Nav.Link>
+                                <Nav.Link href='/Medicamentos'><Button variant='secondary'> Medicamentos</Button ></Nav.Link>
+                                <Nav.Link href='/Beleza'><Button variant='secondary' >Beleza</Button ></Nav.Link>
                                 <Nav className='test'>
-                                    <Nav.Link href="/"><BsFillHouseFill size='1.5rem' color='white' /></Nav.Link>
-                                    <Nav.Link href="/Cadastro"><BsPersonFill size='1.5rem' color='white' /></Nav.Link>
-                                    <Nav.Link href="/Carrinho"><BsCart2 size='1.5rem' color='white' /> </Nav.Link>
+                                    <Nav.Link href='/'><BsFillHouseFill size='1.5rem' color='white' /></Nav.Link>
+                                    <Nav.Link href='/Cadastro'><BsPersonFill size='1.5rem' color='white' /></Nav.Link>
+                                    <Nav.Link href='/Carrinho'><BsCart2 size='1.5rem' color='white' /> </Nav.Link>
+                                    <Nav.Link href='/Login'><LiaAndroid size='2rem' color='white' /></Nav.Link>
+
 
                                 </Nav>
                             </Nav>
