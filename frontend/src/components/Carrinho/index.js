3ca8b1c5ad1aea317142451d676d6c71fcef81b4
Isabellaizira <izidoroisabella@gmail.com>
2023-11-09 16:09:08 -0300

    09-11

--- frontend/src/components/Carrinho/index.js
+++ frontend/src/components/Carrinho/index.js
@@ -1,18 +1,45 @@
 import './carrinho.css'
 import '../Header/header.css'
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 import Container from 'react-bootstrap/Container';
 
 export default function Carrinho() {
+  const [produtos, setProdutos] = useState([])
+  useEffect(() => {
+    const minhaLista = localStorage.getItem('@produtocarrinho')
+    setProdutos(JSON.parse(minhaLista) || [])
+  }, [])
+
+  function excluirProduto(id) {
+    let filtroProdutos = produtos.filter((item) => {
+      return (item.id !== id)
+    })
+    setProdutos(filtroProdutos)
+    localStorage.setItem('@produtocarrinho', JSON.stringify(filtroProdutos))
+
+  }
 
   return (
     <Container fluid>
       <div className='cabecalho'>
         <h1>CARRINHO DE COMPRAS</h1></div>
+      <div>
+        {produtos.map((produtos) => {
+          return (
+            <div>
+              <li key={produtos.id}>
+              </li>
+            </div>
+            
+          )
+        })}
 
 
-    <h1></h1>
+
+        
+      </div>
     </Container>
   )
 }
