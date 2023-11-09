233f226ea16d1b6a9da2e4fb5a82bd7948c08b0c
Isabellaizira <izidoroisabella@gmail.com>
2023-11-09 14:33:37 -0300

    09-11

--- backend/src/Controllers/Carrinho/ListarProdutoCarrinhoController.ts
+++ backend/src/Controllers/Carrinho/ListarProdutoCarrinhoController.ts
@@ -4,6 +4,7 @@ import { ListarProdutoCarrinhoServices } from '../../Services/Carrinho/ListarPro
 class ListarProdutoCarrinhoController {
 
     async handle(req: Request, res: Response) {
+        const id = req.produto_id
         const listarProdutoCarrinhoServices = new ListarProdutoCarrinhoServices()
         const resposta = await listarProdutoCarrinhoServices.execute()
         return res.json(resposta)
