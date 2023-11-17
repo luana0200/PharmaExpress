e8c5f6f7268b4d8d42c5eed1815860febc6fbdbb
Isabellaizira <izidoroisabella@gmail.com>
2023-11-17 14:20:22 -0300

    1711

--- backend/prisma/migrations/20231117171310_/migration.sql
+++ backend/prisma/migrations/20231117171310_/migration.sql
@@ -21,10 +21,9 @@ CREATE TABLE `produtos` (
     `provider` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
     `banner` VARCHAR(191) NOT NULL,
+    `categoriaId` VARCHAR(191) NOT NULL,
     `create_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `update_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `categoriaId` VARCHAR(191) NOT NULL,
-    `carrinhoId` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -42,6 +41,23 @@ CREATE TABLE `categorias` (
 -- CreateTable
 CREATE TABLE `carrinho` (
     `id` VARCHAR(191) NOT NULL,
+    `numero` VARCHAR(191) NOT NULL,
+    `create_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `order_id` VARCHAR(191) NOT NULL,
+    `produtos_id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Order` (
+    `id` VARCHAR(191) NOT NULL,
+    `table` INTEGER NOT NULL,
+    `status` BOOLEAN NOT NULL DEFAULT false,
+    `draft` BOOLEAN NOT NULL DEFAULT true,
+    `final` BOOLEAN NOT NULL DEFAULT false,
+    `name` VARCHAR(191) NULL,
     `create_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     `update_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
 
@@ -52,4 +68,7 @@ CREATE TABLE `carrinho` (
 ALTER TABLE `produtos` ADD CONSTRAINT `produtos_categoriaId_fkey` FOREIGN KEY (`categoriaId`) REFERENCES `categorias`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `produtos` ADD CONSTRAINT `produtos_carrinhoId_fkey` FOREIGN KEY (`carrinhoId`) REFERENCES `carrinho`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `carrinho` ADD CONSTRAINT `carrinho_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `carrinho` ADD CONSTRAINT `carrinho_produtos_id_fkey` FOREIGN KEY (`produtos_id`) REFERENCES `produtos`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/src/Controllers/Categorias/ListarCategoriasController.ts
+++ backend/src/Controllers/Categorias/ListarCategoriasController.ts
@@ -1,22 +1,21 @@
 import { Request, Response } from 'express'
 import { ListarCategoriasServices } from '../../Services/Categorias/ListarCategoriasServices'
 
-
 class ListarCategoriasController {
     async handle(req: Request, res: Response) {
         const listarCategoriasServices = new ListarCategoriasServices()
-        const response = await listarCategoriasServices.execute()
+        //const response = await listarCategoriasServices.execute()
 
-        return res.json(response)
+        return res.json()
     }
 
-//      async findCategoryName(req: Request, res: Response) {
-//          const {categoriaId} = req.params
-//          const listarCategoriasServices = new ListarCategoriasServices()
-//          const response = await listarCategoriasServices.findCategoryName({categoriaId})
+    //  async findCategoryName(req: Request, res: Response) {
+    //      const {categoriaId} = req.params
+    //      const listarCategoriasServices = new ListarCategoriasServices()
+    //      const response = await listarCategoriasServices.findCategoryName({categoriaId})
 
-//          return res.json(response)
-//      }
+    //      return res.json(response)
+    //  }
  }
 
 export { ListarCategoriasController }
