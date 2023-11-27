4187455277321bbddd76d38fd93d9c90c39348ca
lorraynecheerry <lorraynesabino554@gmail.com>
2023-11-27 16:52:42 -0300

    apagado

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import multer from 'multer'
 import uploadConfig from './config/multer'
-
 import { isAutenticado } from './middleware/isAutenticado'
 
 //Criar
@@ -10,22 +9,15 @@ import { AuthLoginController } from './Controllers/Login/AuthLoginController'
 import { CriarCategoriasController } from './Controllers/Categorias/CriarCategoriasController'
 import { CriarProdutosController } from './Controllers/Produtos/CriarProdutosController'
 
-//DESCOMENTAR
-//import { CriarCarrinhoController } from './Controllers/Carrinho/CriarCarrinhoController'
-
-
 //Listar
 import { ListarUsuariosTokenController } from './Controllers/Usuarios/ListarUsuarioController'
 import { ListarProdutosController } from './Controllers/Produtos/ListarProdutosController'
 import { ListarCategoriasController } from './Controllers/Categorias/ListarCategoriasController'
 import { ListarCategoriasUnicoController } from './Controllers/Categorias/ListarCategoriasUnicoController'
-import { ListarProdutoCarrinhoController } from './Controllers/Carrinho/ListarProdutoCarrinhoController'
 
 //Deletar
 import { DeletarUsuariosController } from './Controllers/Usuarios/DeletarUsuariosController'
 import { ListarProdutosUnicoController } from './Controllers/Produtos/ListarProdutosUnicoController'
-
-
 
 const router = Router()
 const upload = multer(uploadConfig.upload('./tmp'))
@@ -43,29 +35,10 @@ router.post('/CriarCategorias', new CriarCategoriasController().handle)
 router.get('/ListarCategorias', new ListarCategoriasController().handle)
 router.get('/ListarCategoriasUnico/:id', new ListarCategoriasUnicoController().handle)
 
-
 //produtos
 router.post('/CriarProdutos', upload.single('file'), new CriarProdutosController().handle)
-router.get('/ListarProdutos', new ListarProdutosController().handle)
+router.get('/ListarProdutos', isAutenticado, new ListarProdutosController().handle)
 router.get('/ListarPdtUnico', new ListarProdutosUnicoController().handle)
-// router.get('/categoryName/:id', new ListarCategoriasController().findCategoryName)
-
-//carrinho
-
-// router.post('/CriarCarrinho', new CriarCarrinhoController().handle)
-router.get('/ListarCarrinho/${id}', new ListarProdutoCarrinhoController().handle)
-
-
-
-//outer.post('/CriarCarrinho', new CriarCarrinhoController().handle)
-
-
-router.get('/ListarCarrinho', new ListarProdutoCarrinhoController().handle)
-
-
-//order
-//router.post('/CriarOrder', new CriarOrderController().handle)
-
 
 export { router }
 
